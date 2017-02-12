34cd6caa53cb50c341e06ed714483fa65cc7488e
aboveyou00 <brandonyoyoslade@gmail.com>
2017-02-12 15:20:09 -0700

    feat(match-cards): add initial scene for MatchCards game

---
+++ MatchCards_Feb10/Assets/Scripts/TitleController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleController : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        SceneManager.LoadScene("MatchCardsScene");
+    }
+
+    private void OnGUI()
+    {
+        int screenWidth = Screen.width;
+        int screenHeight = Screen.height;
+        int left = (screenWidth / 2) - 100;
+        int top = (screenHeight / 2) + 200;
+        int width = 200, height = 60;
+        if (GUI.Button(new Rect(left, top, width, height), "Tap to Continue"))
+        {
+            SceneManager.LoadScene("MatchCardsScene");
+        }
+    }
+}
