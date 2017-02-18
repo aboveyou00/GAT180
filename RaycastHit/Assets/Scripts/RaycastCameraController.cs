c6526d5e370096895f47beb87626d8c68d07547c
aboveyou00 <brandonyoyoslade@gmail.com>
2017-02-17 17:26:27 -0700

    feat(raycast-hit): add sample raycast hit test

---
+++ RaycastHit/Assets/Scripts/RaycastCameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastCameraController : MonoBehaviour
+{
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+        if (Physics.Raycast(ray, out hitInfo) && Input.GetMouseButtonDown(0))
+        {
+            GameObject obj = hitInfo.collider.gameObject;
+            var rigidbody = obj.GetComponent<Rigidbody>();
+            rigidbody.useGravity = true;
+            rigidbody.AddForceAtPosition(ray.direction * 50, hitInfo.point, ForceMode.Impulse);
+        }
+    }
+}
