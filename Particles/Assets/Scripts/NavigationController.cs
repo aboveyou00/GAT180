566cb07fd7ee0d7993e4d3e25a825af9cdf31b38
aboveyou00 <brandonyoyoslade@gmail.com>
2017-02-22 11:45:52 -0700

    feat(particles): add particles project with five particle scenes

---
+++ Particles/Assets/Scripts/NavigationController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NavigationController : MonoBehaviour
+{
+    public Scene nextScene;
+    public float secondsUntilNavigation;
+
+    private float navigationTime;
+
+    void Start()
+    {
+        navigationTime = Time.time + secondsUntilNavigation;
+    }
+    
+    void Update()
+    {
+        if (Time.time > navigationTime)
+        {
+            SceneManager.LoadScene(nextScene);
+        }
+    }
+}
---
+++ Particles/Assets/Scripts/PartyHornController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class PartyHornController : MonoBehaviour
+{
+    public AudioClip clip;
+    public float offsetTime;
+
+    private AudioSource source;
+    private float startTime;
+    private bool hasStarted;
+
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        startTime = Time.time + offsetTime;
+        hasStarted = false;
+    }
+    
+    void Update()
+    {
+        if (!hasStarted && Time.time > startTime)
+        {
+            hasStarted = true;
+            source.PlayOneShot(clip);
+        }
+    }
+}
