026d5e75c3c0971459c4af8c07d8ec13aeee6e76
aboveyou00 <brandonyoyoslade@gmail.com>
2017-03-08 10:17:36 -0700

    fix(finaltd): generate the first wave with more than 0 hp

--- FinalTowerDefence/Assets/Algorithms/WaveGenerator.cs
+++ FinalTowerDefence/Assets/Algorithms/WaveGenerator.cs
@@ -25,7 +25,7 @@ public class WaveGenerator
     {
         var count = (int)Math.Pow(5 * ((idx + 5) / 5.0f), .8);
         var speed = 3 + (idx / 10.0f);
-        var hp = (int)(10 * Math.Pow(idx, 1.4));
+        var hp = (int)(10 * Math.Pow(idx + 2, 1.4));
         return new Wave(idx, count, speed, hp);
     }
 }
