03a01e307f8d4695e38b8a1da3c9c84619a12945
aboveyou00 <brandonyoyoslade@gmail.com>
2017-03-08 18:47:02 -0700

    feat(finaltd): add 'Game Over' screen

--- FinalTowerDefence/Assets/Scripts/GameController.cs
+++ FinalTowerDefence/Assets/Scripts/GameController.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
     public MapController map;
     public WaveController waves;
     public ShopController shop;
+
+    public Scene loseScene;
+    public bool hasLost = false;
 
     public float playSpeed = 1;
 
@@ -20,10 +24,53 @@ public class GameController : MonoBehaviour
         map.game = this;
         waves.game = this;
         shop.game = this;
+
+        DontDestroyOnLoad(this);
     }
 
     private void Update()
     {
-        timeSurvived += Time.deltaTime;
+        if (!hasLost)
+        {
+            timeSurvived += Time.deltaTime;
+            if (checkForGameOver())
+            {
+                hasLost = true;
+                Time.timeScale = 1;
+                SceneManager.LoadScene(loseScene);
+            }
+        }
+    }
+    private bool checkForGameOver()
+    {
+        return shop.money < 0;
+    }
+
+    private void OnGUI()
+    {
+        if (!hasLost) return;
+
+        float left = 0, right = Screen.width;
+        float top = 0;
+
+        GUI.Label(new Rect(left + 4, top + 4, right - left - 8, 40), "You lost! You went into debt.");
+        top += 24;
+
+        GUI.Label(new Rect(left + 4, top + 4, right - left - 8, 40), "Money Spent: $" + moneysSpent);
+        top += 24;
+        GUI.Label(new Rect(left + 4, top + 4, right - left - 8, 40), "Enemies Killed: " + enemiesKilled);
+        top += 24;
+        GUI.Label(new Rect(left + 4, top + 4, right - left - 8, 40), "Enemies Banished: " + enemiesBanished);
+        top += 24;
+        GUI.Label(new Rect(left + 4, top + 4, right - left - 8, 40), "Time survived: " + (int)Mathf.Floor(timeSurvived) + "s");
+        top += 24;
+
+        if (GUI.Button(new Rect(left + 4, top + 4, right - left - 8, 40), "Restart")) restartGame();
+        top += 24;
+    }
+    private void restartGame()
+    {
+        SceneManager.LoadScene(0, LoadSceneMode.Single);
+        Destroy(gameObject);
     }
 }
---
+++ FinalTowerDefence/Assets/Serialization/Scene.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Code adapted from http://answers.unity3d.com/answers/1204071/view.html
+//Purpose: allow the unity editor to serialize scenes
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[System.Serializable]
+public class Scene
+{
+    [SerializeField]
+    private Object m_SceneAsset;
+    [SerializeField]
+    private string m_SceneName = "";
+    public string SceneName
+    {
+        get {
+            return m_SceneName;
+        }
+    }
+
+    // makes it work with the existing Unity methods (LoadLevel/LoadScene)
+    public static implicit operator string(Scene sceneField)
+    {
+        return sceneField.SceneName;
+    }
+}
+
+#if UNITY_EDITOR
+[CustomPropertyDrawer(typeof(Scene))]
+public class SceneFieldPropertyDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect _position, SerializedProperty _property, GUIContent _label)
+    {
+        EditorGUI.BeginProperty(_position, GUIContent.none, _property);
+        SerializedProperty sceneAsset = _property.FindPropertyRelative("m_SceneAsset");
+        SerializedProperty sceneName = _property.FindPropertyRelative("m_SceneName");
+        _position = EditorGUI.PrefixLabel(_position, GUIUtility.GetControlID(FocusType.Passive), _label);
+        if (sceneAsset != null)
+        {
+            sceneAsset.objectReferenceValue = EditorGUI.ObjectField(_position, sceneAsset.objectReferenceValue, typeof(SceneAsset), false);
+            if (sceneAsset.objectReferenceValue != null)
+            {
+                sceneName.stringValue = (sceneAsset.objectReferenceValue as SceneAsset).name;
+            }
+        }
+        EditorGUI.EndProperty();
+    }
+}
+#endif
