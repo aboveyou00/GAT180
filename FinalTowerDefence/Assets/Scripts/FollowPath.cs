e217988b97210a35503231014f5e58c66847504c
aboveyou00 <brandonyoyoslade@gmail.com>
2017-03-13 09:06:28 -0600

    feat(finaltd): rotate enemies while they are following the path

--- FinalTowerDefence/Assets/Scripts/FollowPath.cs
+++ FinalTowerDefence/Assets/Scripts/FollowPath.cs
@@ -8,6 +8,8 @@ public class FollowPath : MonoBehaviour
     public float startDelay, speed;
     public MapController map;
     public bool restartWhenDone = false;
+    public bool setOrientation = false;
+    public float offsetOrientation;
     public Vector2 offsetPosition;
 
     private int currentIdx = 0;
@@ -47,11 +49,32 @@ public class FollowPath : MonoBehaviour
                 }
             }
         }
-        Vector2 pt;
-        if (currentProgress == 0) pt = ptv(path[currentIdx]);
-        else pt = mix(path[currentIdx], path[currentIdx + 1], currentProgress);
+        Vector2 pt, lookAtPos = new Vector2();
+        if (currentProgress == 0)
+        {
+            pt = ptv(path[currentIdx]);
+        }
+        else
+        {
+            pt = mix(path[currentIdx], path[currentIdx + 1], currentProgress);
+            if (setOrientation)
+            {
+                var nextPathPt = path[currentIdx + 1];
+                lookAtPos = new Vector2(nextPathPt.x + (offsetPosition.x / 60), nextPathPt.y + (offsetPosition.y / 60));
+                lookAtPos = new Vector2(map.offset.x + (lookAtPos.x * map.tileSize.x), map.offset.y + (lookAtPos.y * map.tileSize.y));
+            }
+        }
         pt += new Vector2(offsetPosition.x / 60, offsetPosition.y / 60);
         transform.position = new Vector3(map.offset.x + (pt.x * map.tileSize.x), map.offset.y + (pt.y * map.tileSize.y), 0);
+        if (setOrientation)
+        {
+            Debug.DrawLine(lookAtPos - new Vector2(5, 5), lookAtPos + new Vector2(5, 5));
+            Debug.DrawLine(lookAtPos - new Vector2(5, -5), lookAtPos + new Vector2(5, -5));
+            Debug.DrawLine(transform.position, lookAtPos);
+            var dir = (Vector3)lookAtPos - transform.position;
+            var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle + offsetOrientation, Vector3.forward);
+        }
         
         if (!wasDone && done)
         {
